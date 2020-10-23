e2663516d967070ca4ff5297d4ab5edcfb770cb6
Vytautas Sipavičius <vspalevenele@gmail.com>
2020-10-23 13:08:08 +0300

    Added reading and coloring pages

---
+++ InteractiveTextbook/InteractiveTextbook/Controllers/ReadController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InteractiveTextbook.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace InteractiveTextbook.Controllers
+{
+    public class ReadController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ReadController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Textbookss
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Textbooks.ToListAsync());
+        }
+
+        public IActionResult Textbook(int textbookId, int pageNumber)
+        {
+            var page = _context.Pages.Include(x => x.Textbook).FirstOrDefault(x => x.TextbookId == textbookId && x.PageNumber == pageNumber);
+            if (page == null)
+            {
+                page = _context.Pages.Include(x => x.Textbook).FirstOrDefault(x => x.TextbookId == textbookId);
+            }
+            return View(page);
+        }
+    }
+}
---
+++ InteractiveTextbook/InteractiveTextbook/Models/TextBookPageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using InteractiveTextbook.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InteractiveTextbook.Models
+{
+    public class TextBookPageViewModel
+    {
+        public int PageNumber { get; set; }
+
+        public string Text { get; set; }
+
+        public int TextbookId { get; set; }
+
+        public virtual Textbook Textbook { get; set; }
+    }
+}
